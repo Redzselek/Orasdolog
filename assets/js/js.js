c56620915d7f695f3362fe6157c43d809c6c24b7
Egyedi Róbert <egyedirobika@gmail.com>
2023-10-18 11:14:50 +0200

    Kesz

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,6 +1,45 @@
-var ido = document.getElementById("ora").value
-var formazott = parseInt(ido.split(":"))
+var ora=perc=mp = 0;
+console.log(ido);
 
-setInterval(() => {
-    
-}, formazott);
+
+function Indit() {
+    var t = document.getElementById("ora").value
+var ido = t.split(':')
+    if(ido.length == 3)
+{
+    ora=ido[0]
+    perc=ido[1]
+    mp=ido[2]
+}
+else if(ido.length == 2)
+{
+    perc=ido[0]
+    mp=ido[1]
+}
+if(ido.length == 1)
+{
+    mp=ido[0]
+}
+
+    var s = setInterval(() => {
+        document.getElementById("ido").innerHTML = ora+":"+perc+":"+mp
+        
+        if (mp!=0) {
+            mp--
+        }
+        else if (perc != 0) {
+            perc--
+            mp += 59
+        }
+        else{
+            ora--
+            perc += 59
+            mp += 59
+        }
+        if (ora == 0 && perc == 0 && mp == 0) {
+            clearInterval(s)
+            document.getElementById("ido").innerHTML = "0:0:0"
+            alert("bazdmeg")
+        }
+    }, 1000);
+}
